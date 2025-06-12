367c06bedc0f7ae0e8f10b1af3788d6377166358
René Schünemann <rene.schuenemann@sap.com>
2025-06-12 10:45:28 +0200

    status conditions refactoring

--- internal/controller/reconcile.go
+++ internal/controller/reconcile.go
@@ -12,9 +12,7 @@ import (
 	"github.com/openmcp-project/controller-utils/pkg/logging"
 	core "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	apimeta "k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/api/resource"
-	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -37,10 +35,16 @@ func (r *LandscaperReconciler) reconcile(ctx context.Context, req ctrl.Request) 
 		return reconcile.Result{}, err
 	}
 
+	var status *reconcileStatus
+
 	if ls.DeletionTimestamp.IsZero() {
-		res, err = r.handleCreateUpdateOperation(ctx, ls)
+		res, status, err = r.handleCreateUpdateOperation(ctx, ls)
 	} else {
-		res, err = r.handleDeleteOperation(ctx, ls)
+		res, status, err = r.handleDeleteOperation(ctx, ls)
+	}
+
+	if status != nil {
+		status.convertToLandscaperStatus(&ls.Status)
 	}
 
 	updateErr := r.updateStatus(ctx, ls)
@@ -53,287 +57,155 @@ func (r *LandscaperReconciler) reconcile(ctx context.Context, req ctrl.Request) 
 }
 
 func (r *LandscaperReconciler) handleCreateUpdateOperation(ctx context.Context,
-	ls *v1alpha1.Landscaper) (reconcile.Result, error) {
+	ls *v1alpha1.Landscaper) (reconcile.Result, *reconcileStatus, error) {
 	log := logging.FromContextOrPanic(ctx)
 
+	status := newCreateOrUpdateStatus(ls.GetGeneration())
+
 	if err := r.ensureFinalizer(ctx, ls); err != nil {
-		return reconcile.Result{}, err
+		return reconcile.Result{}, status, err
 	}
 
 	if err := r.checkReconcileAnnotation(ctx, ls); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	if err := r.observeGeneration(ctx, ls); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	if ls.Status.Phase == v1alpha1.Ready {
-		return reconcile.Result{}, nil
+		return reconcile.Result{}, status, err
 	}
 
 	if err := r.ensureInstanceID(ctx, ls); err != nil {
-		return reconcile.Result{}, err
+		return reconcile.Result{}, status, err
+	}
+
+	providerConfig, err := r.getProviderConfigForLandscaper(ctx, ls, r.PlatformCluster)
+	if err != nil {
+		log.Error(err, "failed to get provider config for landscaper instance")
+		status.setInstallProviderConfigError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	req := reconcile.Request{NamespacedName: client.ObjectKeyFromObject(ls)}
 	res, err := r.ClusterAccessReconciler.Reconcile(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to reconcile cluster access for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ClusterAccessError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setInstallClusterAccessError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	if res.RequeueAfter > 0 {
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "WaitingForClusterAccessToBeReady",
-			Message:            "MCP and/or Workload Clusters are not yet ready",
-		})
-		return res, nil
+		status.setInstallWaitForClusterAccessReady()
+		return res, status, nil
 	}
 
 	mcpCluster, err := r.ClusterAccessReconciler.MCPCluster(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to get MCP cluster for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "MCPClusterError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setInstallClusterAccessError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	workloadCluster, err := r.ClusterAccessReconciler.WorkloadCluster(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to get Workload cluster for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "WorkloadClusterError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
-	}
-
-	initConditions(ls)
-
-	providerConfig, err := r.getProviderConfigForLandscaper(ctx, ls, r.PlatformCluster)
-	if err != nil {
-		log.Error(err, "failed to get provider config for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ProviderConfigError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setInstallClusterAccessError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	conf, err := r.createConfig(ls, mcpCluster, workloadCluster, providerConfig)
 	if err != nil {
 		log.Error(err, "failed to create configuration for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ConfigurationError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setInstallConfigurationError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	if err := instance.InstallLandscaperInstance(ctx, conf); err != nil {
 		log.Error(err, "failed to install landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Installed",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "InstallFailed",
-			Message:            err.Error(),
-		})
-		return ctrl.Result{}, err
+		status.setInstallFailed(err)
+		return ctrl.Result{}, status, err
 	}
 	log.Debug("landscaper instance has been installed")
-	apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-		Type:               "Installed",
-		Status:             meta.ConditionTrue,
-		ObservedGeneration: ls.Generation,
-		Reason:             "LandscaperInstalled",
-		Message:            "Landscaper has been installed",
-	})
+	status.setInstalled()
 
 	if readinessCheckResult := instance.CheckReadiness(ctx, conf); !readinessCheckResult.IsReady() {
 		log.Debug("landscaper instance is not yet ready")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Ready",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "WaitingForLandscaperToBecomeReady",
-			Message:            readinessCheckResult.Message(),
-		})
-		return ctrl.Result{RequeueAfter: 40 * time.Second}, nil
+		status.setWaitForReadinessCheck(readinessCheckResult)
+		return ctrl.Result{RequeueAfter: 40 * time.Second}, status, nil
 	}
 
 	ls.Status.Phase = v1alpha1.Ready
 	log.Debug("landscaper instance has become ready")
-	apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-		Type:               "Ready",
-		Status:             meta.ConditionTrue,
-		ObservedGeneration: ls.Generation,
-		Reason:             "LandscaperReady",
-		Message:            "Landscaper is ready",
-	})
+	status.setReady()
 
-	return reconcile.Result{}, nil
+	return reconcile.Result{}, status, nil
 }
 
-func (r *LandscaperReconciler) handleDeleteOperation(ctx context.Context, ls *v1alpha1.Landscaper) (reconcile.Result, error) {
+func (r *LandscaperReconciler) handleDeleteOperation(ctx context.Context, ls *v1alpha1.Landscaper) (reconcile.Result, *reconcileStatus, error) {
 	log := logging.FromContextOrPanic(ctx)
 
-	if err := r.ensurePhaseTerminating(ctx, ls); err != nil {
-		log.Error(err, "failed to ensure landscaper instance is in terminating phase")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "PhaseError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
-	}
+	status := newDeleteStatus(ls.GetGeneration())
 
 	req := reconcile.Request{NamespacedName: client.ObjectKeyFromObject(ls)}
 	res, err := r.ClusterAccessReconciler.Reconcile(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to reconcile cluster access for landscaper instance deletion")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ClusterAccessError",
-			Message:            err.Error(),
-		})
-		return res, err
+		status.setUninstallClusterAccessError(err)
+		return res, status, err
 	}
 
 	if res.RequeueAfter > 0 {
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "WaitingForClusterAccessToBeReady",
-			Message:            "MCP and/or Workload Clusters are not yet ready",
-		})
-		return res, nil
+		status.setUninstallWaitForClusterAccessReady()
+		return res, status, nil
 	}
 
 	mcpCluster, err := r.ClusterAccessReconciler.MCPCluster(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to get MCP cluster for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "MCPClusterError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setUninstallClusterAccessError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	workloadCluster, err := r.ClusterAccessReconciler.WorkloadCluster(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to get Workload cluster for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "WorkloadClusterError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setUninstallClusterAccessError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	providerConfig, err := r.getProviderConfigForLandscaper(ctx, ls, r.PlatformCluster)
 	if err != nil {
 		log.Error(err, "failed to get provider config for landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ProviderConfigError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setUninstallProviderConfigError(err)
+		return reconcile.Result{}, status, err
 	}
 
 	conf, err := r.createConfig(ls, mcpCluster, workloadCluster, providerConfig)
 	if err != nil {
 		log.Error(err, "failed to create configuration to uninstall landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ConfigurationError",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setUninstallConfigurationError(err)
+		return reconcile.Result{}, status, err
 	}
 
-	if err := instance.UninstallLandscaperInstance(ctx, conf); err != nil {
+	if err = instance.UninstallLandscaperInstance(ctx, conf); err != nil {
 		log.Error(err, "failed to uninstall landscaper instance")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "UninstallFailed",
-			Message:            err.Error(),
-		})
-		return reconcile.Result{}, err
+		status.setUninstallFailed(err)
+		return reconcile.Result{}, status, err
 	}
 	log.Debug("landscaper instance has been uninstalled")
-	apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-		Type:               "Uninstalled",
-		Status:             meta.ConditionTrue,
-		ObservedGeneration: ls.Generation,
-		Reason:             "LandscaperUninstalled",
-		Message:            "Landscaper has been uninstalled",
-	})
+	status.setUninstalled()
 
 	result, err := r.ClusterAccessReconciler.ReconcileDelete(ctx, req)
 	if err != nil {
 		log.Error(err, "failed to reconcile cluster access for landscaper instance deletion")
-		apimeta.SetStatusCondition(&ls.Status.Conditions, meta.Condition{
-			Type:               "Uninstalled",
-			Status:             meta.ConditionFalse,
-			ObservedGeneration: ls.Generation,
-			Reason:             "ClusterAccessError",
-			Message:            err.Error(),
-		})
-		return result, err
+		status.setUninstallClusterAccessError(err)
+		return result, status, err
 	}
 
 	if result.RequeueAfter > 0 {
-		return result, nil
+		return result, status, nil
 	}
 
-	if err := r.removeFinalizer(ctx, ls); err != nil {
-		return reconcile.Result{}, err
+	if err = r.removeFinalizer(ctx, ls); err != nil {
+		return reconcile.Result{}, status, err
 	}
 
-	return reconcile.Result{}, nil
+	return reconcile.Result{}, status, nil
 }
 
 func (r *LandscaperReconciler) updateStatus(ctx context.Context, ls *v1alpha1.Landscaper) error {
@@ -369,42 +241,13 @@ func (r *LandscaperReconciler) removeFinalizer(ctx context.Context, ls *v1alpha1
 	return nil
 }
 
-func (r *LandscaperReconciler) observeGeneration(ctx context.Context, ls *v1alpha1.Landscaper) error {
-	if ls.Status.ObservedGeneration != ls.Generation {
-		ls.Status.ObservedGeneration = ls.Generation
-		ls.Status.Phase = v1alpha1.Progressing
-		if err := r.OnboardingCluster.Client().Status().Update(ctx, ls); err != nil {
-			log := logging.FromContextOrPanic(ctx)
-			log.Error(err, "failed to update observed generation for landscaper resource")
-			return fmt.Errorf("failed to update observed generation for landscaper resource %s/%s: %w", ls.Namespace, ls.Name, err)
-		}
-	}
-	return nil
-}
-
 func (r *LandscaperReconciler) checkReconcileAnnotation(ctx context.Context, ls *v1alpha1.Landscaper) error {
-	if controller.HasAnnotationWithValue(ls, v1alpha1.LandscaperOperation, v1alpha1.OperationReconcile) && ls.Status.Phase == v1alpha1.Ready {
+	if controller.HasAnnotationWithValue(ls, v1alpha1.LandscaperOperation, v1alpha1.OperationReconcile) {
 		log := logging.FromContextOrPanic(ctx)
-		ls.Status.Phase = v1alpha1.Progressing
-		if err := r.updateStatus(ctx, ls); err != nil {
-			log.Error(err, "failed to handle reconcile annotation: unable to change phase of landscaper resource to progressing")
-			return fmt.Errorf("failed to handle reconcile annotation: unable to change phase of landscaper resource %s/%s to progressing: %w", ls.Namespace, ls.Name, err)
-		}
 
 		if err := controller.EnsureAnnotation(ctx, r.OnboardingCluster.Client(), ls, v1alpha1.LandscaperOperation, v1alpha1.OperationReconcile, true, controller.DELETE); err != nil {
 			log.Error(err, "failed to remove reconcile annotation from landscaper resource")
 			return fmt.Errorf("failed to remove reconcile annotation from landscaper resource %s/%s: %w", ls.Namespace, ls.Name, err)
-		}
-	}
-	return nil
-}
-
-func (r *LandscaperReconciler) ensurePhaseTerminating(ctx context.Context, ls *v1alpha1.Landscaper) error {
-	if ls.Status.ObservedGeneration != ls.Generation || ls.Status.Phase != v1alpha1.Terminating {
-		ls.Status.ObservedGeneration = ls.Generation
-		ls.Status.Phase = v1alpha1.Terminating
-		if err := r.updateStatus(ctx, ls); err != nil {
-			return err
 		}
 	}
 	return nil
