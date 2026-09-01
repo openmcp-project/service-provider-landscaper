d0f25f0d42758688b531b72e9fd4a12ed9438755
René Schünemann (SAP) <rene.schuenemann@sap.com>
2026-09-01 16:12:00 +0200

    chore: enable fips verification
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- cmd/service-provider-landscaper/main.go
+++ cmd/service-provider-landscaper/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/openmcp-project/service-provider-landscaper/cmd/service-provider-landscaper/app"
+
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 )
 
 func main() {
+	fips.Verify(context.Background())
+
 	cmd := app.NewServiceProviderLandscaperCommand()
 
 	if err := cmd.Execute(); err != nil {
